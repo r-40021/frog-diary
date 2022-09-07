057377ca42efb5a5c6786dab24f38cf64bd861fe
Frog <75155258+r-40021@users.noreply.github.com>
2022-09-07 20:42:37 +0900

    Add: Article list & page

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -23,3 +24,5 @@ const app = initializeApp(firebaseConfig);
 if (typeof window !== "undefined") {
   const analytics = getAnalytics(app);
 }
+
+export const db = getFirestore(app);
