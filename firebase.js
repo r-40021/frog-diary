781ed4e1860509a0c1de47c439b9cf03a1b81ccf
Frog <75155258+r-40021@users.noreply.github.com>
2022-09-07 18:56:15 +0900

    Add: Firebase

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWIlkoPzlDeGAd0_gqZ85574qvNyoNYLQ",
+  authDomain: "frog-dairy.firebaseapp.com",
+  projectId: "frog-dairy",
+  storageBucket: "frog-dairy.appspot.com",
+  messagingSenderId: "624017035589",
+  appId: "1:624017035589:web:ad7047aac66dbcb9ebee2c",
+  measurementId: "G-SD1PZ6T1N0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ブラウザのみで動作させる
+if (typeof window !== "undefined") {
+  const analytics = getAnalytics(app);
+}
