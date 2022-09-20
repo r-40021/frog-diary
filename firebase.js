98d3752c4e073b291e78983df3c19c9c30603de0
Frog <frog@frogapp.net>
2022-09-21 00:02:57 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
@@ -19,10 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// ブラウザのみで動作させる
-if (typeof window !== "undefined") {
-  const analytics = getAnalytics(app);
-}
 
 export const db = getFirestore(app);
